90e52495c013c434ee855ad4fe6778d195bedadd
nspeer12 <nspeer252@gmail.com>
2021-06-30 13:51:45 -0400

    search

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import threading
 import multiprocessing
 import time
 from fastapi import FastAPI, Request, Query
+from fastapi.templating import Jinja2Templates
+from typing import Optional
 import requests
 import json
 import os
@@ -11,19 +13,27 @@ from assistant import *
 
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 dex = DexterCloud(debug=True)
 
 
 @app.get('/')
-async def index():
-	return 'dexter'
+def index(request:Request, query: Optional[str] = None):
+
+	ans = None
+	if query:
+		print(query)
+		ans = dex.process_input(query)
+		print(ans)
+		
+	return templates.TemplateResponse('index.html', {"request": request, "ans": ans})
 
 
 @app.get('/query/{query}')
 async def fulfillment(query:str):
 	print(query)
-	if query is not None:
+	if query:
 		print(query)
 		res = dex.process_input(query)
 		print(res)
